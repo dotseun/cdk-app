bb50548ce4adc818ed7d08712ecba70e4c0a0148
segun <oaa10.me@gmail.com>
2021-09-20 20:06:02 +0100

    hello commit

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -2,8 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { MybucketappStack } from '../lib/cdk-app-stack';
+import { PipelineStack } from '../lib/cdk-pipeline-stack';
 
 const app = new cdk.App();
 new MybucketappStack(app, 'MybucketappStack', {
+  env: { account: '412554673201', region: 'eu-west-2' },
+});
+
+new PipelineStack(app, 'PipelineStack', {
   env: { account: '412554673201', region: 'eu-west-2' },
 });
